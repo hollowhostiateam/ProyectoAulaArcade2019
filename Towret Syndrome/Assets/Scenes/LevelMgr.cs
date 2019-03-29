0840da81e57f3ed62ba35f8a32eb772112b065bf
xVanCooper <49079718+xVanCooper@users.noreply.github.com>
2019-03-29 22:21:30 +0100

    Initial Commit

---
+++ Towret Syndrome/Assets/Scenes/LevelMgr.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelMgr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CargaNivel(string pNombreNivel)
+    {
+        SceneManager.LoadScene(pNombreNivel);
+    }
+}
